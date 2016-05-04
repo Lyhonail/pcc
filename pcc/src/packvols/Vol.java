0bb3a9d4ee7706d610e2c2572b4cd7468b4a502c
user <user@172.17.61.108>
2016-05-04 11:04:41 +0200

    les vols2

--- pcc/src/packvols/Vol.java
+++ pcc/src/packvols/Vol.java
@@ -1,35 +1,56 @@
 package packvols;
 
 import java.util.Date;
+import java.util.Hashtable;
+import java.util.Iterator;
+import packhoraires.Horaire;
 
 public class Vol {
 
     private String num_vol;
-    private String type_vol;
-    private String dest_or_prov;
-    private int heure;
-    private int minute;
-    private Object avion;
+    private Horaire horaire;
+    private Avion avion;
+    private static Hashtable <String, Vol> lesVols = new Hashtable<String, Vol>();
+
+    public Vol(String num_vol, Horaire horaire, Avion avion) {
+        this.num_vol = num_vol;
+        this.horaire = horaire;
+        this.avion = avion;
+        lesVols.put(num_vol, this);
+    }
+    
 
     public void getNum_vol() {
     }
 
-    public void getType_vol() {
-    }
-
-    public void getdest_or_prov() {
-    }
-
-    public void getHeure() {
-    }
-
 
     public String toString() {
-        String info = "";
+        String info = "Numero de vol : " + num_vol +"|avion :"+avion +" |horaire: " + horaire + "\n Liste  des vols du jour pour un avions  : \n";
+        Iterator it = lesVols.values().iterator();
+        while (it.hasNext()) {
+            Vol v = (Vol) it.next();
+            info += v.toString() + "\n";
+        }
         return info;
     }
 
     public void afficher() {
         System.out.println(this.toString());
     }
+    
+    /*
+    public class numvolInvalide extends Exception {
+
+        private String numvol;
+
+        public numvolInvalide(String numvol) {
+            this.numvol = numvol;
+        }
+
+        public String toString() {
+            return "numero de vol" + numvol + "  est inexistante !!";
+        }
+
+    }
+*/
 }
