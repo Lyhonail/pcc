a8a329652f5b8c512b32c11ace0d0db8ccf5cfbe
user <user@172.17.61.108>
2016-05-04 10:33:23 +0200

    les vols

---
+++ pcc/src/packvols/VolArrivee.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packvols;
+
+import java.util.ArrayList;
+import packhorraires.Horaire;
+
+/**
+ *
+ * @author user
+ */
+public class VolArrivee extends Vol {
+
+    String provenance;
+    ArrayList<VolArrivee> lesVolsArrivee;
+
+    public VolArrivee(String num_vol, Horaire horaire, Avion avion, String prov) {
+        super(num_vol, horaire, avion);
+        provenance = prov;
+        lesVolsArrivee = new ArrayList<VolArrivee>();
+    }
+
+}
---
+++ pcc/src/packvols/VolDepart.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packvols;
+
+import java.util.ArrayList;
+import packhorraires.Horaire;
+
+/**
+ *
+ * @author user
+ */
+public class VolDepart extends Vol {
+    String depart;
+    ArrayList<VolDepart> lesVolsArrivee;
+    
+    
+    public VolDepart(String num_vol, Horaire horaire, Avion avion,String dep) {
+        super(num_vol, horaire, avion);
+                depart = dep;
+                lesVolsArrivee  = new ArrayList<VolDepart>();
+    }
+    
+}
